ce27d4c7d8975647d88d1a7de382b0f863493da1
Branches . <branchesdossantosdavi@gmail.com>
2026-03-30 20:22:30 -0300

    .

--- Atividades/Tuplas, Listas e Dicionários/Dicionarios/5.py
+++ Atividades/Tuplas, Listas e Dicionários/Dicionarios/5.py
@@ -2,14 +2,14 @@ import time
 import sys
 
 
-agenda = {"contato": {"nome": "Ana", "telefone": "1111-1111"},
-          "contato1": {"nome": "Bruno", "telefone": "2222-2222"}}
+agenda = {"contato1": {"nome": "Ana", "telefone": "1111-1111"},
+          "contato2": {"nome": "Bruno", "telefone": "2222-2222"}}
 
 print("Adicionar, Atualizar, Remover ou Lista Ordenada")
 print("Oque fazer?")
 r = str(input())
 
-if r == "adicionar" or "Adicionar":
+if r == "adicionar" or r == "Adicionar":
     nome = input("Insira seu nome: ")
     cont = input("Insira seu número: ")
 
@@ -40,16 +40,46 @@ if r == "adicionar" or "Adicionar":
     agenda["contato2"] = {"nome": nome, "telefone": cont}
     print("Contato adicionado", agenda["contato2"])
 
-elif r == "atualizar" or "Atualizar":
-    print("Qual contato atualizar? (ID)")
-    r = int(input())
-
-    print(agenda[r])
-
-    print("Oque editar?")
+elif r == "atualizar" or r == "Atualizar":
+    print("Qual contato atualizar? (Ex: contato1)")
     r1 = str(input())
 
+    print(agenda[r1])
+
+    print("Oque editar? (nome ou telefone)")
+    r = str(input())
+
     if r == "nome" or r == "Nome":
-        nome = input("Insira o novo nome: ")
-        agenda[r, {"nome": nome}] = nome
+        r = input("Insira o novo nome: ")
+        agenda[r1, ["nome", r]]
+        print(agenda)
+
+    elif r == "telefone" or r == "Telefone":
+        r = input("Insira o novo telefone: ")
+
+        cont = str(r)
+        cont = list(cont)
+
+        if cont[-5] != "-":            # Adiciona "-" xxxxxx-xxxx
+            cont.insert(-4, "-")
+    
+        if cont[2] != " ":             # Adiciona " " xx xxxx-xxxx
+            cont.insert(2, " ")
+    
+        if cont[3] != "9":               # Adiciona "9" xx 9xxxx-xxxx
+            cont.insert(3, "9")
+
+        nc = len(cont)    
+        if nc == 13:                   # Para formato xx 9xxxx-xxxx '13 caracteres'
+
+            cont.insert(2, ")")        # Para formato (xx) 9xxxx-xxxx
+            cont.insert(0, "(")
+
+        else:
+            print("O número está errado")
+            sys.exit()
+    
+        cont = "".join(cont)
+
+        agenda[r1, {"telefone": cont}]
         print(agenda)
