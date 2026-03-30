4960a522d986b3b6ab615fe2551bc2913926f7dd
Branches . <branchesdossantosdavi@gmail.com>
2026-03-30 20:00:21 -0300

    .

--- Atividades/Tuplas, Listas e Dicionários/Dicionarios/5.py
+++ Atividades/Tuplas, Listas e Dicionários/Dicionarios/5.py
@@ -19,27 +19,37 @@ if r == "adicionar" or "Adicionar":
 
     if cont[-5] != "-":            # Adiciona "-" xxxxxx-xxxx
         cont.insert(-4, "-")
-        print(cont)
     
     if cont[2] != " ":             # Adiciona " " xx xxxx-xxxx
         cont.insert(2, " ")
-        print(cont)
     
     if cont[3] != "9":               # Adiciona "9" xx 9xxxx-xxxx
         cont.insert(3, "9")
-        print(cont)
-        print(len(cont))
 
     nc = len(cont)    
     if nc == 13:                   # Para formato xx 9xxxx-xxxx '13 caracteres'
 
         cont.insert(2, ")")        # Para formato (xx) 9xxxx-xxxx
         cont.insert(0, "(")
-        print(cont)
 
     else:                
         print("O número está errado")
         sys.exit()
     
+    cont = "".join(cont)
     agenda["contato2"] = {"nome": nome, "telefone": cont}
-    print("Nome:")
+    print("Contato adicionado", agenda["contato2"])
+
+elif r == "atualizar" or "Atualizar":
+    print("Qual contato atualizar? (ID)")
+    r = int(input())
+
+    print(agenda[r])
+
+    print("Oque editar?")
+    r1 = str(input())
+
+    if r == "nome" or r == "Nome":
+        nome = input("Insira o novo nome: ")
+        agenda[r, {"nome": nome}] = nome
+        print(agenda)
