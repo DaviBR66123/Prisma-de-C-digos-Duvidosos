a04ca9aa92448343078da046dca3d658a5a4038d
Branches . <branchesdossantosdavi@gmail.com>
2026-04-03 22:08:01 -0300

    Dificuldade com essa funcões

---
+++ Atividades/Def/5.py
@@ -0,0 +1,10 @@
+def contar_letras(r):
+    strlista = list(strlista)
+    r = len(strlista)
+    return r
+
+strlista = []
+strlista.append(input("Digite uma palavra: "))
+
+r = contar_letras(strlista)
+print(f"A palavra possui {r} letras")
